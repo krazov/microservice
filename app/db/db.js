451e757d5f8dd026f9fcd62578e6d462f327f071
Jon Krazov <krazov@interia.pl>
2020-06-15 21:16:09 +0200

    Adding missing semicolon

--- app/db/db.js
+++ app/db/db.js
@@ -7,7 +7,7 @@ mongoose
 mongoose
     .connection
     .once('open', function mongooseOpenHandler() {
-        console.log('Connection to the database has been established')
+        console.log('Connection to the database has been established');
     })
     .on('error', function mongooseErrorHandler() {
         console.error.bind(console, 'connection error:');
